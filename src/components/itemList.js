654640c8f3ea7be4ee1c580f45891dc9b19b696f
Geoffrey Leung <geo.leung112@gmail.com>
2021-08-16 16:23:41 -0400

    initial commit

---
+++ src/components/itemList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Item from './item';
+
+const ItemList = ({todos, setTodos, filteredTodos}) => {
+    return(
+        <div className="todo-container">
+            <ul className="todo-list">
+                {filteredTodos.map(todo => (
+                    <Item 
+                    key={todo.id} 
+                    text={todo.text}
+                    setTodos={setTodos} 
+                    todos={todos}
+                    todo={todo}
+                />
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default ItemList;
